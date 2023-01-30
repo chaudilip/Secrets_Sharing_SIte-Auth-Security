9a66aad55b35809f563de79c591f26bfc98f19d8
chaudilip <dilipchau3602@gmail.com>
2023-01-30 17:54:00 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,79 @@
+//jshint esversion:6
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const app = express();
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+
+mongoose.connect("mongodb://localhost:27017/userDB",{useNewUrlParser:true});
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
+
+const userSchema = new mongoose.Schema({
+    email:String,
+    password:String
+});
+
+
+
+
+const User = new mongoose.model("User", userSchema);
+
+
+app.get("/",function(req,res){
+    res.render("home");
+});
+
+app.get("/login",function(req,res){
+    res.render("login");
+});
+
+app.get("/register",function(req,res){
+    res.render("register");
+});
+
+app.post("/register",function(req,res){
+    bcrypt.hash(req.body.password, saltRounds
+        , function(err, hash) {
+        const newUser = new User({
+            email:req.body.username,
+            password:hash
+        });
+        newUser.save(function(err){
+            if(!err){
+                res.render("secrets");
+            }else{
+                console.log("Failed to Insert ");
+            }
+        });
+    });
+   
+});
+
+app.post("/login",function(req,res){
+    const username = req.body.username;
+    const password = req.body.password;
+    User.findOne({email:username},function(err,foundUser){
+        if(err){
+            console.log(err);
+        }else{
+            if(foundUser){
+                bcrypt.compare(password, foundUser.password, function(err, result) {
+                    if(result === true){
+                        res.render("secrets");
+                    }
+                });
+            }
+        }
+    });
+});
+
+
+app.listen(3000,function(){
+    console.log("listening on port 3000");
+})
